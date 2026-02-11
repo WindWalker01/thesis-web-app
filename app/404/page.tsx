6ecfc72dfe9459a18669961606fc79a3dbbee4f9
Ruzzel Mendoza <ruzzelmendozadev@gmail.com>
2026-02-11 22:41:12 +0800

    simple authorization and authentication

---
+++ app/404/page.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <main>
+      <h1>Oops not Found!</h1>
+    </main>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+
+  async function signIn() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) setError(error.message);
+    else router.push("/dashboard");
+  }
+
+  async function signUp() {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) setError(error.message);
+    else router.push("/dashboard");
+  }
+
+  return (
+    <main>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={signIn}>Sign In</button>
+      <button onClick={signUp}>Sign Up</button>
+
+      {error && <p>{error}</p>}
+    </main>
+  );
+}
