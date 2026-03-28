429682c8f355935d117048acfd4ebfb7be51fb1b
Ruzzel Mendoza <ruzzelmendozadev@gmail.com>
2026-03-28 23:09:40 +0800

    ui inconsistencies

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   /* config options here */
+  /* config options here */
   reactStrictMode: true,
   images: {
-    domains: [
-      "images.unsplash.com",
-      "lh3.googleusercontent.com",
-    ],
+    domains: ["images.unsplash.com", "lh3.googleusercontent.com"],
   },
 };
 
